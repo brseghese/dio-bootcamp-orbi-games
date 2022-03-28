66a81e8b100f724b637e153a8433cc773e9a3568
brseghese <brseghese@gmail.com>
2022-03-28 11:24:56 -0300

    jogabilidade e som

--- 3-jogo-space-shooter/js/script.js
+++ 3-jogo-space-shooter/js/script.js
@@ -1,6 +1,17 @@
 function start() {
-  const playArea = document.querySelector("#game-area");
+  const gameArea = document.querySelector("#gameArea");
   const player = document.querySelector("#player");
+  const alienEnemy = [
+    "./assets/images/alienEnemy1.png",
+    "./assets/images/alienEnemy2.png",
+    "./assets/images/alienEnemy3.png",
+    "./assets/images/alienEnemy4.png",
+    "./assets/images/alienEnemy5.png",
+    "./assets/images/alienEnemy6.png",
+    "./assets/images/alienEnemy7.png",
+    "./assets/images/alienEnemy8.png",
+    "./assets/images/alienEnemy9.png",
+  ];
   let game = {};
   let KEY = {
     W: 87,
@@ -12,6 +23,21 @@ function start() {
   game.pressed = [];
   game.timer = setInterval(loop, 30);
   let canShoot = true;
+  let velocidade = 0;
+
+  let somDisparo = document.getElementById("somDisparo");
+  let somExplosao = document.getElementById("somExplosao");
+  let musica = document.getElementById("musica");
+
+  musica.addEventListener(
+    "ended",
+    function () {
+      musica.currentTime = 0;
+      musica.play();
+    },
+    false
+  );
+  musica.play();
 
   document.addEventListener("keydown", function (e) {
     game.pressed[e.which] = true;
@@ -22,12 +48,12 @@ function start() {
   });
 
   function moveBackgroud() {
-    let leftPosition = getComputedStyle(playArea).getPropertyValue(
+    let leftPosition = getComputedStyle(gameArea).getPropertyValue(
       "background-position-x"
     );
     let position = parseInt(leftPosition);
     position -= 2;
-    playArea.style.backgroundPositionX = `${position}px`;
+    gameArea.style.backgroundPositionX = `${position}px`;
   }
 
   function loop() {
@@ -71,8 +97,8 @@ function start() {
 
     if (game.pressed[KEY.D]) {
       let leftPosition = getComputedStyle(player).getPropertyValue("left");
-      if (leftPosition === "680px") {
-        player.style.left = "680px";
+      if (leftPosition === "630px") {
+        player.style.left = "630px";
       } else {
         let position = parseInt(leftPosition);
         position += 10;
@@ -88,6 +114,7 @@ function start() {
   function shootLaser() {
     if (canShoot == true) {
       canShoot = false;
+      somDisparo.play();
 
       let xPosition = parseInt(
         getComputedStyle(player).getPropertyValue("left")
@@ -97,7 +124,7 @@ function start() {
       );
 
       let laser = document.createElement("div");
-      playArea.appendChild(laser);
+      gameArea.appendChild(laser);
       laser.classList.add("laser");
       laser.style.left = `${xPosition + 100}px`;
       laser.style.top = `${yPosition - 30}px`;
@@ -110,6 +137,24 @@ function start() {
         );
         laser.style.left = `${xPosition + 30}px`;
 
+        //
+
+        let aliens = document.querySelectorAll(".alien");
+        aliens.forEach((alien) => {
+          if (checkCollision(laser, alien)) {
+            xPosition = parseInt(
+              getComputedStyle(alien).getPropertyValue("left")
+            );
+            yPosition = parseInt(
+              getComputedStyle(alien).getPropertyValue("top")
+            );
+            alien.remove();
+            explosion(xPosition, yPosition);
+          }
+        });
+
+        //
+
         if (xPosition > 700) {
           clearInterval(laserInterval);
           laser.remove();
@@ -118,6 +163,91 @@ function start() {
       }
     }
   }
+
+  function createAlien() {
+    let newAlien = document.createElement("img");
+    let alienSprite = alienEnemy[Math.floor(Math.random() * alienEnemy.length)];
+    newAlien.src = alienSprite;
+    newAlien.classList.add("alien");
+    newAlien.classList.add("animationAlien");
+    newAlien.style.left = "710px";
+    newAlien.style.top = `${Math.floor(Math.random() * 510 + 20)}px`;
+    gameArea.appendChild(newAlien);
+    moveAlien(newAlien);
+  }
+
+  function moveAlien(alien) {
+    let moveAlienInterval = setInterval(() => {
+      let xPosition = parseInt(
+        getComputedStyle(alien).getPropertyValue("left")
+      );
+      alien.style.left = `${xPosition - (4 + velocidade)}px`;
+      if (xPosition < 30) {
+        alien.remove();
+        gameOver();
+        // if (Array.from(alien.classList).includes("deadAlien")) {}
+      }
+    }, 30);
+  }
+
+  let alienInterval = setInterval(createAlien, 1500);
+
+  function checkCollision(laser, alien) {
+    let laserTop = parseInt(laser.style.top);
+    let laserLeft = parseInt(laser.style.left);
+    // let laserBottom = laserTop;
+
+    let alienTop = parseInt(alien.style.top);
+    let alienLeft = parseInt(alien.style.left);
+    let alienBottom = alienTop - 80;
+
+    if (laserLeft + 70 >= alienLeft) {
+      // laserLeft <= 700 &&
+      if (laserTop <= alienTop && laserTop >= alienBottom) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  function explosion(xPosition, yPosition) {
+    somExplosao.play();
+    let newExplosion = document.createElement("div");
+    gameArea.appendChild(newExplosion);
+    newExplosion.classList.add("explosion");
+    newExplosion.classList.add("animationExplosion");
+    newExplosion.style.left = `${xPosition}px`;
+    newExplosion.style.top = `${yPosition}px`;
+    let timpeExplosion = setInterval(removeExplosion, 1000);
+
+    function removeExplosion() {
+      newExplosion.remove();
+      clearInterval(timpeExplosion);
+    }
+    velocidade += 0.1;
+    console.log(velocidade);
+  }
 }
 
-start();
+function startGame() {
+  let gameStart = document.getElementById("gameStart");
+  gameStart.style.display = "none";
+  let player = document.getElementById("player");
+  player.style.display = "block";
+
+  start();
+}
+
+function gameOver() {
+  let gameOver = document.getElementById("gameOver");
+  gameOver.style.display = "flex";
+}
+
+function restart() {
+  let gameOver = document.getElementById("gameOver");
+  gameOver.style.display = "none";
+  location.reload();
+}
